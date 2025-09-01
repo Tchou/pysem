2957dca36bbb432b32ca9c39717dfd60488c9b0f
Kim Nguyễn <kim.nguyen@universite-paris-saclay.fr>
2025-09-01 16:01:42 +0200

    WIP encoding.

--- test/tests_mlsem.ml
+++ test/tests_mlsem.ml
@@ -1,90 +1,81 @@
 (* MLsem tests, not OCaml code *)
-(* Get an attribute x from an object *)
 
-type meta_dict ('b) = { get : string -> any .. } & 'b
+(*
+Sandbox for python encoding into types.
+Naming:
 
-(* The type of type, the sstt type for python's type object *)
-type type_type = (type_ where type_ = { __dict__ : () -> meta_dict({ ..});
-                                        __class__ : () -> type_;
-                                        __mro__ : () -> mro_of_type .. }
-                  and mro_of_type = {
-                    __dict__ : () -> meta_dict ({ ..});
-                    __class__ : () -> tuple_type;
-                    __tuple_content__ : () -> [ type_  object_type ] .. }
-                  and tuple_type = {
-                    __dict__ : () -> meta_dict ({ ..});
-                    __class__ : () -> type_;
-                    __mro__ : () -> mro_of_tuple .. }
-                  and mro_of_tuple = {
-                    __dict__ : () -> meta_dict ({ ..});
-                    __class__ : () -> tuple_type;
-                    __tuple_content__ : () -> [ tuple  object_type ] .. }
-                  and object_type = {
-                    __dict__ : () -> meta_dict({ ..});
-                    __class__ : () -> type_;
-                    __mro__ : () -> mro_of_object .. }
-                  and mro_of_object = {
-                    __dict__ : () -> meta_dict ( { ..});
-                    __class__ : () -> tuple_type;
-                    __tuple_content__ : () -> [ object_type ] .. })
+foo_type : is the MLsem type representing Python object named
+foo, that have python type 'type'. So the Python object
+  'int' 'bool' 'list' etc… are represented by MLsem values
+  of type 'int_type', 'bool_type' 'list_type'
 
-(* the sstt type for tuple *)
+foo_val ('a) : is the MLsem type representing Python object of
+python type foo. So the python value '42' has type 'int_val'
+The python value '(1,2)' has type 
 
-type tuple_type = (tuple_type where type_ = { __dict__ : () -> meta_dict({ ..});
-                                              __class__ : () -> type_;
-                                              __mro__ : () -> mro_of_type .. }
-                   and mro_of_type = {
-                     __dict__ : () -> meta_dict ({ ..});
-                     __class__ : () -> tuple_type;
-                     __tuple_content__ : () -> [ type_  object_type ] .. }
-                   and tuple_type = {
-                     __dict__ : () -> meta_dict ({ ..});
-                     __class__ : () -> type_;
-                     __mro__ : () -> mro_of_tuple .. }
-                   and mro_of_tuple = {
-                     __dict__ :  () -> meta_dict ({ ..});
-                     __class__ : () -> tuple_type;
-                     __tuple_content__ : () -> [ tuple  object_type ] .. }
-                   and object_type = {
-                     __dict__ : () -> meta_dict({ ..});
-                     __class__ : () -> type_;
-                     __mro__ : () -> mro_of_object .. }
-                   and mro_of_object = {
-                     __dict__ : () -> meta_dict ( { ..});
-                     __class__ : () -> tuple_type;
-                     __tuple_content__ : () -> [ object_type ] .. })
-
-
-(* the type of a polymorphic tuple *)
-type tuple ('c) = { __dict__ : () -> meta_dict ( { ..});
-                    __class__ : () -> tuple_type;
-                    __tuple_content__ : () -> ('c & [any*]) .. }
-
-(* type of a polymorphic type *)
-type type_ ('c) = { __dict__ : () -> meta_dict ( { ..});
-                    __class__ : () -> type_type; 
-                    __mro__ : () -> tuple('c)   ..}
-(* type representing a python object with direct attributes 'a and 
-   mro :'c 
 *)
-type object('a, 'c) = { __dict__ : () -> meta_dict('a); 
-                        __class__: () -> type_('c)  .. }
-type any_object = object ({ ..}, [type_type+])
 
-type direct_x ('a) = object ({ x : 'a .. }, [type_type+])
+
+type meta_dict ('b) = Dict({ __get : string -> any .. } & 'b)
+
+(* The type of type, the sstt type for python's type object. To 
+   bootstrap everything, we define monomorphic versions first
+*)
+type type_type = Py({ __dict__ : () -> meta_dict({ ..});
+                   __class__ : () -> type_type;
+                   __mro__ : () -> type_type_mro;
+                   __ty__ : () -> Type  .. })
+and type_type_mro = Py({
+  __dict__ : () -> meta_dict ({ ..});
+  __class__ : () -> tuple_type;
+  __ty__ : () -> [ type_type  object_type ] .. })
+and tuple_type = Py({
+  __dict__ : () -> meta_dict ({ ..});
+  __class__ : () -> type_type;
+  __mro__ : () -> tuple_type_mro; 
+  __ty__ : () -> Type    .. })
+and tuple_type_mro = Py({
+  __dict__ : () -> meta_dict ({ ..});
+  __class__ : () -> tuple_type;
+  __ty__ : () -> [ tuple_type  object_type ] .. })
+and object_type = Py({
+  __dict__ : () -> meta_dict({ ..});
+  __class__ : () -> type_type;
+  __mro__ : () -> object_type_mro; 
+  __ty__ : () -> Type .. })
+and object_type_mro = Py({
+  __dict__ : () -> meta_dict ( { ..});
+  __class__ : () -> tuple_type;
+  __ty__ : () -> [ object_type ] .. })
+
+(* polymorphic type interface  *)
+type object ('mro, 'att, 'ty) = Py({
+  __dict__ : () -> meta_dict ('att);
+  __class__ : () -> type_('mro);
+  __ty__ : () -> 'ty
+.. })
+and type_ ('mro) = Py({
+  __dict__ : () -> meta_dict ({ ..});
+  __class__ : () -> type_type;
+  __mro__ : () -> tuple ({ ..}, 'mro);
+  __ty__ : () -> Type
+.. })
+and tuple ('att, 'ty) = Py({
+  __dict__ : () -> meta_dict ('att);
+  __class__ : () -> tuple_type;
+  __ty__ : () -> 'ty
+.. })
+
+
+type direct_x ('a) = object (any, { x : 'a .. }, any)
 type x_in_mro ('a) = [ (~ direct_x (any))* (direct_x ('a))  type_type* ]
-type attr_x ('a) = direct_x ('a) | object ({ .. }\{x : any ..}, x_in_mro('a))
+type attr_x ('a) = direct_x ('a) | object (x_in_mro('a), { .. }, any)
 val get_x : attr_x ('a) -> 'a
 
-type direct_y ('a) = object ({ y : 'a .. }, [type_type+])
+type direct_y ('a) = object (any, { y : 'a .. }, any)
 type y_in_mro ('a) = [ (~ direct_y (any))* (direct_y ('a))  type_type* ]
-type attr_y ('a) = direct_y ('a) | object ({ .. }\{y : any ..}, y_in_mro('a))
+type attr_y ('a) = direct_y ('a) | object (y_in_mro('a), { .. }, any)
 val get_y : attr_y('a) -> 'a
 
-let f a = get_x a
+let f a = (get_x a) + (get_y a)
 
-(* set field *)
-let g (self : any_object) = 
-  let d = { (self.__dict__ ()) with c = 42 } in
-  let f (_ : ()) = d in
-  { self with __dict__ = f }
