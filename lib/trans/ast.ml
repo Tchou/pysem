f0e61ce492ce4d89da8fa44c5c1887e2e460c2cd
Philippe Volte--Vieira <philippe.volte-vieira@universite-paris-saclay.fr>
2025-11-21 15:14:05 +0100

    print block hints

--- lib/trans/ast.ml
+++ lib/trans/ast.ml
@@ -205,7 +205,7 @@ let rec pp_instr' fmt instr' : unit =
   let pp_instr_list il =
     pp_print_list ~pp_sep:(fun fmt () -> fprintf fmt "@\n") pp_instr il in
   match instr' with
-  | Block il -> pp_instr_list fmt il
+  | Block il -> fprintf fmt "@[# Block [@\n%a# ] Block@\n@]" pp_instr_list il
   | Assign (x,e) -> fprintf fmt "@[<hov 2>%a := %a@]"
                        (pp_ident) x pp_expr e
   | FunDef (i,s,b) -> fprintf fmt "@[<hov 2>def %a%a:@\n%a@]"
