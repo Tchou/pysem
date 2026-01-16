33ba1a160259e537b9e2a1200da9c5593694f008
Philippe Volte--Vieira <philippe.volte-vieira@universite-paris-saclay.fr>
2026-01-16 01:45:34 +0100

    formatting

--- bin/main.ml
+++ bin/main.ml
@@ -20,7 +20,7 @@ let options = Arg.align []
 
 let ml_type mcenv ast =
   let annot = MS.Reconstruction.infer mcenv
-      (MS.Refinement.refinement_envs mcenv ast) ast in
+                (MS.Refinement.refinement_envs mcenv ast) ast in
   MSC.typeof_def mcenv annot ast
   |> MTS.norm_and_simpl
 
@@ -54,51 +54,51 @@ let main () =
   Arg.parse options add_input_file usage_message;
   match !input_file with
   | None ->
-    Format.eprintf "%s: missing file@\n%s" Sys.argv.(0)
-      (Arg.usage_string options usage_message)
+     Format.eprintf "%s: missing file@\n%s" Sys.argv.(0)
+       (Arg.usage_string options usage_message)
   | Some file ->
-    let open Format in
-    let m, bil, to_loc = Parsing.parse ~file in
-    (* dbg_pr "pyre-parsed expression" "%a" *)
-    (* Sexplib0.Sexp.pp_hum (PC.Module.sexp_of_t m); *)
-    (* pr "block_infos" "%a" *)
-    (* (pp_print_list ~pp_sep:pp_print_space Parsing.pp_block_info) bil; *)
+     let open Format in
+     let m, bil, to_loc = Parsing.parse ~file in
+     (* dbg_pr "pyre-parsed expression" "%a" *)
+     (* Sexplib0.Sexp.pp_hum (PC.Module.sexp_of_t m); *)
+     (* pr "block_infos" "%a" *)
+     (* (pp_print_list ~pp_sep:pp_print_space Parsing.pp_block_info) bil; *)
 
-    let p = Prog.of_module (Env.init bil to_loc) m in
-    dbg_pr "pysem ast" "%a" Ast.pp_prog p;
+     let p = Prog.of_module (Env.init bil to_loc) m in
+     dbg_pr "pysem ast" "%a" Ast.pp_prog p;
 
-    let ml = Prog.to_ml p in
-    dbg_pr "mlsem ast" "%a"
-      (pp_print_list ~pp_sep:pp_print_newline pp_ml_top) ml;
+     let ml = Prog.to_ml p in
+     dbg_pr "mlsem ast" "%a"
+       (pp_print_list ~pp_sep:pp_print_newline pp_ml_top) ml;
 
-    let ms_exprs = List.map (fun (v,t) -> v, ML.Transform.transform t) ml in
+     let ms_exprs = List.map (fun (v,t) -> v, ML.Transform.transform t) ml in
 
-    (* MS.Config.infer_overload := false; *)
-    let tt, mce, names =
-      try List.fold_left treat_def (0.,MC.Env.empty, []) ms_exprs
-      with
-      | MSC.Untypeable err ->
-        Format.printf "@[<hov>%s:@\n@ @[<hov 2>%a@]@]" err.title
-          (Format.pp_print_option pp_print_string) err.descr;
-        raise (MSC.Untypeable err)
-    in
-    pr "reconstruction environement"
-      "%a@\n@{<yellow;italic>checked in %.2fms@}"
-      (let nl = ref true in
-       pp_print_list
-         ~pp_sep:(fun fmt _ -> if !nl then fprintf fmt "@\n"; nl := true)
-         ( fun fmt v ->
-           let s = MC.Env.find v mce in
-           if MlVar.show v |> Utils.is_internal |> not
-           then Format.fprintf fmt "@[<hov>%a: %a@]"
-                  MlVar.pp v
-                  Py_params.pp_py_scheme s
-           else if Utils.debug
-           then Format.fprintf fmt "@[<hov>%a@]"
-                  Printing.pp_ml_tys (v, s)
-           else nl := false ))
-      (List.rev names)
-      (ms_of_us tt)
+     (* MS.Config.infer_overload := false; *)
+     let tt, mce, names =
+       try List.fold_left treat_def (0.,MC.Env.empty, []) ms_exprs
+       with
+       | MSC.Untypeable err ->
+          Format.printf "@[<hov>%s:@\n@ @[<hov 2>%a@]@]" err.title
+            (Format.pp_print_option pp_print_string) err.descr;
+          raise (MSC.Untypeable err)
+     in
+     pr "reconstruction environement"
+       "%a@\n@{<yellow;italic>checked in %.2fms@}"
+       (let nl = ref true in
+        pp_print_list
+          ~pp_sep:(fun fmt _ -> if !nl then fprintf fmt "@\n"; nl := true)
+          ( fun fmt v ->
+            let s = MC.Env.find v mce in
+            if MlVar.show v |> Utils.is_internal |> not
+            then Format.fprintf fmt "@[<hov>%a: %a@]"
+                   MlVar.pp v
+                   Py_params.pp_py_scheme s
+            else if Utils.debug
+            then Format.fprintf fmt "@[<hov>%a@]"
+                   Printing.pp_ml_tys (v, s)
+            else nl := false ))
+       (List.rev names)
+       (ms_of_us tt)
 
 let () =
   if Unix.isatty Unix.stdout then Colors.add_ansi_marking Format.std_formatter;
@@ -106,21 +106,21 @@ let () =
   try
     if Utils.debug
     then begin
-      MT.Recording.start_recording ();
-      main () |> fst;
-      MT.Recording.stop_recording ();
-      MT.Recording.(tally_calls () |> save_to_file "tally_calls");
-    end
+        MT.Recording.start_recording ();
+        main () |> fst;
+        MT.Recording.stop_recording ();
+        MT.Recording.(tally_calls () |> save_to_file "tally_calls");
+      end
     else main () |> fst
   with
   | Parsing.Syntax (file, e) ->
-    Format.eprintf "%s: %d:%d-%d:%d : %s@\n"
-      file e.line e.column e.end_line e.end_column e.message;
-    exit 3
+     Format.eprintf "%s: %d:%d-%d:%d : %s@\n"
+       file e.line e.column e.end_line e.end_column e.message;
+     exit 3
   | Sys_error msg -> Format.eprintf "%s@\n" msg; exit 1
   | MSC.Untypeable _ -> exit 2 (* printed above *)
   | e ->
-    Format.eprintf "ERROR: %s@\n%s@\n"
-      (Printexc.to_string e)
-      (Printexc.get_backtrace ());
-    exit 10
+     Format.eprintf "ERROR: %s@\n%s@\n"
+       (Printexc.to_string e)
+       (Printexc.get_backtrace ());
+     exit 10
