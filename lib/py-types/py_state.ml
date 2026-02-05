bf480bf0acf8214038ca04dbadbd969d7847140d
Philippe Volte--Vieira <philippe.volte-vieira@universite-paris-saclay.fr>
2026-02-05 15:43:14 +0100

    pp pystate

--- lib/py-types/py_state.ml
+++ lib/py-types/py_state.ml
@@ -267,3 +267,40 @@ and of_param {pos;_} = match pos with
   | [] -> Var (mk_ident ()) |> Ast.dannot
   | [ e ] -> of_expr e
   | _ -> failwith "TODO not just 1 pos parameter"
+
+let show_res_kind = function R -> "R" | V -> "V"
+let pp_res_kind fmt r = Format.fprintf fmt "%s" (show_res_kind r)
+
+let pp_ident fmt = function
+  | Simple mlv -> Format.fprintf fmt "@[%%%s@]" (Printing.mlvar_show mlv)
+  | Source id -> Ast.Ident.pp fmt id
+
+let rec pp_expr' fmt e =
+  let open Format in
+  match e with
+  | Const c -> Ast.pp_const fmt c
+  | Var id -> pp_ident fmt id
+  | Res (r, e) -> fprintf fmt "@[<hov 2>%a(%a)@]" pp_res_kind r pp_expr e
+  | Proj (r, e) -> fprintf fmt "@[<hov 2>(%a).%a@]" pp_expr e pp_res_kind r
+  | IfNotRes {cond;v;s;body} ->
+    fprintf fmt "@[<hov 2>bind %a, %a =@ %a in@ %a@]"
+      pp_ident v pp_ident s pp_expr cond pp_expr body
+  | Val {cond; v; s; body} ->
+    fprintf fmt "@[<hov 2>bindv %a, %a =@ %a in %a@]"
+      pp_ident v pp_ident s pp_expr cond pp_expr body
+  | Ite (e1, e2, e3) ->
+    fprintf fmt
+      "@[@[<hov 2>if %a@]@\n@[<hov 2>then %a@]@\n@[<hov 2>else %a@]@]@\n"
+      pp_expr e1 pp_expr e2 pp_expr e3
+  | Tuple el ->
+    fprintf fmt "@[<hov 1>(%a)@]" (Printing.pp_list ~sep:"," pp_expr) el
+  | Pi (i, e) -> fprintf fmt "@[π%d(%a)@]" i pp_expr e
+  | EmptyRec -> fprintf fmt "{}"
+  | RecUpdate (e1, id, e2) ->
+    fprintf fmt "@[{ %a@ @[with %a = %a @]}@]" pp_expr e1 pp_ident id pp_expr e2
+  | Field (e, id) -> fprintf fmt "@[%a@,.%a@]" pp_expr e pp_ident id
+  | Lambda (id, b, e) ->
+    fprintf fmt "@[<hov 2>%s %a.@ %a@]"
+      (if b then "ƛ" else "λ") pp_ident id pp_expr e
+  | App (e1, e2) -> fprintf fmt "@[<hov 2>%a@ %a@]" pp_expr e1 pp_expr e2
+and pp_expr fmt (_,e) = pp_expr' fmt e
